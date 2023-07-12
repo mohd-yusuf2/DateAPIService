b7941e8333448364623e09d75b2b9b5227cfe25a
Yusuf154 <yusufjava8@gmail.com>
2023-07-12 20:26:16 +0530

    initial commit

---
+++ src/main/java/com/ipm/das/DateApiServiceApplication.java
@@ -0,0 +1,15 @@
+package com.ipm.das;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.ipm.util.IpmUtil;
+
+@SpringBootApplication
+public class DateApiServiceApplication {
+  IpmUtil ipm=new IpmUtil();
+	public static void main(String[] args) {
+		SpringApplication.run(DateApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ipm/das/DateApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipm.das;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
