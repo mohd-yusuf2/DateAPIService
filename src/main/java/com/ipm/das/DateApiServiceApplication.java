5a37f3e24d5697a427bd6e703283af12736d1ff5
766448 <766448@LTIN260215.cts.com>
2023-07-13 10:47:48 +0530

    updated

--- src/main/java/com/ipm/das/DateApiServiceApplication.java
+++ src/main/java/com/ipm/das/DateApiServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.ipm.util.IpmUtil;
 
 @SpringBootApplication
-public class DateApiServiceApplication {
+public class DateApiServiceApplication { 
   IpmUtil ipm=new IpmUtil();
 	public static void main(String[] args) {
 		SpringApplication.run(DateApiServiceApplication.class, args);
